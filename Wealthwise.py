a7db5e170d9d632b5ba786ce25688ce9157c2a9f
abhiyadav135 <abhiyadav010305@gmail.com>
2024-10-04 23:09:50 +0530

    modified:   Wealthwise.py

--- Wealthwise.py
+++ Wealthwise.py
@@ -1,1 +1,18 @@
-print("Hello ")
+from flask import Flask,request,render_template
+import mysql.connector
+
+# Establishing the connection
+cnx=mysql.connector.connect(
+    user='username'
+    password='password'
+    host='127.0.0.1')
+
+# Creating a cursor
+cur=cnx.cursor()
+
+
+app=Flask(_name_)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
